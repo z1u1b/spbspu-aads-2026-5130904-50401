7909f49289cb626ea2e40e367e942b505d884920
zub <ars1for1work@gmail.com>
2026-05-20 15:25:33 +0300

    delete: delete swap function to node

--- zubarev.arsenii/S4/my_node_tree.hpp
+++ zubarev.arsenii/S4/my_node_tree.hpp
@@ -16,8 +16,6 @@ namespace zubarev
     BSTreeNode< Key, Value >* left_;
     BSTreeNode< Key, Value >* right_;
     BSTreeNode< Key, Value >* parent_;
-
-    void swap(BSTreeNode& rhs) noexcept;
   };
   template < class Key, class Value >
   BSTreeNode< Key, Value >::BSTreeNode():
@@ -27,14 +25,7 @@ namespace zubarev
   BSTreeNode< Key, Value >::BSTreeNode(Key key, Value val):
     data_(std::make_pair(key, val))
   {}
-  template < class Key, class Value >
-  void BSTreeNode< Key, Value >::swap(BSTreeNode& rhs) noexcept
-  {
-    std::swap(data_, rhs.data_);
-    std::swap(left_, rhs.left_);
-    std::swap(right_, rhs.right_);
-    std::swap(parent_, rhs.parent_);
-  }
+
 };
 
 #endif
